df763336b463187ab51c819b77557e3ef1f465d0
mrzinofc <145413073+mrzinofc@users.noreply.github.com>
2024-05-24 13:47:59 -0300

    att script

--- script.js
+++ script.js
@@ -1,48 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let menuIcon = document.getElementById('menu-icon');
-    let navbar = document.querySelector('.navbar');
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('header nav a');
-    let contactForm = document.getElementById('contact-form');
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    window.onscroll = () => {
-        sections.forEach(sec => {
-            let top = window.scrollY;
-            let offset = sec.offsetTop - 150;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute('id');
+    var form = event.target;
+    var formData = new FormData(form);
 
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(link => {
-                    link.classList.remove('active');
-                });
-                document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
-            }
-        });
-    };
-
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('active');
-    };
-
-    // Adicionando evento de envio do formulário
-    contactForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-
-        let formData = new FormData(contactForm);
-
-        try {
-            let response = await fetch('/', { // O Netlify redirecionará automaticamente os dados do formulário para o endpoint correto
-                method: 'POST',
-                body: formData
-            });
-
-            let data = await response.json();
-            console.log(data); // Você pode fazer algo com a resposta do servidor, como exibir uma mensagem para o usuário
-        } catch (error) {
-            console.error('Erro ao enviar formulário:', error);
-            // Você pode exibir uma mensagem de erro para o usuário aqui
+    fetch('/', {
+        method: 'POST',
+        body: formData,
+    }).then(response => {
+        if (response.ok) {
+            alert('Formulário enviado com sucesso!');
+            form.reset();
+        } else {
+            alert('Ocorreu um erro ao enviar o formulário.');
         }
+    }).catch(error => {
+        console.error('Erro ao enviar formulário:', error);
+        alert('Ocorreu um erro ao enviar o formulário.');
     });
 });
